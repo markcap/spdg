590af3b66492000bfa05187c9e2ebf0eb7cb6680
Mark Capodagli <pimea.mark@gmail.com>
2010-01-14 13:25:59 -0600

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,2 @@
+ActionController::Base.session = { :session_key => '_AZLCXW_session', :secret => 'DXCBINUMOTFZGSBBDMOQCVAFBCBRTXVIDFYDEAAU' }
+ActionController::Base.session_store = :active_record_store
