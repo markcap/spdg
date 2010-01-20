885b903032acc16b02e8c71a37a7d74efe7b8453
Mark Capodagli <pimea.mark@gmail.com>
2010-01-20 14:52:18 -0600

    added fck text editor for new posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,10 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(params[:article])
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Successfully created article."
-      redirect_to @article
+      redirect_to articles_path
     else
       render :action => 'new'
     end
